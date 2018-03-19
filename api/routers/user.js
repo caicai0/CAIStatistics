1f99d229392fde0abc0a1ce0b553f599375b2259
caicai <972008514@qq.com>
2018-03-19 17:25:58 +0800

    学建模

--- api/routers/user.js
+++ api/routers/user.js
@@ -1,6 +1,6 @@
 
 const router = require('koa-router')();
-const models = require('../models');
+const orm = require('../orm/models/index');
 const md5 = require('md5');
 
 router.post('/user/register', async(ctx, next) => {
@@ -9,11 +9,11 @@ router.post('/user/register', async(ctx, next) => {
         pass: ctx.request.body.password,
         repeatpass: ctx.request.body.repeatpass
     };
-    let user = await models.User.findOne({where:{username:post.name}});
+    let user = await orm.User.findOne({where:{username:post.name}});
     if (user) {
         ctx.body = {code: 0, message: "用户已存在"};
     }else {
-        user = await models.User.create({username: post.name, passwordMd5: post.pass});
+        user = await orm.User.create({username: post.name, passwordMd5: post.pass});
         if(user){
             ctx.body = {code: 0, message: "注册成功"};
         }
@@ -26,11 +26,11 @@ router.post('/user/login', async (ctx, next) => {
         pass: ctx.request.body.password,
         repeatpass: ctx.request.body.repeatpass
     };
-    let user = await models.User.findOne({where:{username:post.name}});
+    let user = await orm.User.findOne({where:{username:post.name}});
     if (user) {
         ctx.body = {code: 0, message: "用户已存在"};
     }else {
-        user = await models.User.create({username: post.name, passwordMd5: post.pass});
+        user = await orm.User.create({username: post.name, passwordMd5: post.pass});
         if(user){
             ctx.body = {code: 0, message: "注册成功"};
         }
