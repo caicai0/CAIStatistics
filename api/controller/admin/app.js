247cd3112f0885cf74e165eb4e86906567640b7e
caicai <916903976@qq.com>
2018-05-08 17:33:07 +0800

    应用列表

--- api/controller/admin/app.js
+++ api/controller/admin/app.js
@@ -2,6 +2,33 @@
 const orm = require('../../orm/models/index');
 const jsonToken = require('../../service/jsonToken');
 
+const userApplicationList = async function (ctx) {
+    const header = {
+        token:ctx.request.headers.token
+    };
+    const post = {
+        page:ctx.request.body.page,
+        size:ctx.request.body.size
+    };
+    try {
+        const payload = await jsonToken.verify(header.token);
+        if (jsonToken.isExpire(payload.iat)){
+            if (!post.page) {
+                post.page = 0;
+            }
+            if (!post.size) {
+                post.size = 20;
+            }
+            const list = await orm.userApplication.find({where:{userId:payload.userId},offset:post.page*post.size,limit:post.size});
+            ctx.body = JSON.stringify({code:0,data:list});
+        } else {
+            ctx.body = JSON.stringify({code:2,message:'token过期'});
+        }
+    }catch (e) {
+        ctx.body = JSON.stringify({code:1,error:e});
+    }
+};
+
 const createApp = async function (ctx) {
     const header = {
         token:ctx.request.headers.token
