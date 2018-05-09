c57a6c5e577e4e49cac2b5bb13adf5d6cba452af
caicai <916903976@qq.com>
2018-05-09 13:07:07 +0800

    基本信息

--- api/controller/admin/baseInfo.js
+++ api/controller/admin/baseInfo.js
@@ -44,11 +44,81 @@ let create = async function (ctx) {
 };
 
 let del = async function (ctx) {
-
+    const header = {
+        token:ctx.request.headers.token
+    };
+    const post = {
+        userApplicationId:ctx.request.body.userApplicationId,
+        name:ctx.request.body.name,
+        classPath:ctx.request.body.classPath,
+        functionName:ctx.request.body.functionName
+    };
+    try {
+        const payload = await jsonToken.verify(header.token);
+        if (jsonToken.isExpire(payload.iat)){
+            if (post.userApplicationId && post.name && post.classPath && post.functionName && post.keyPath) {
+                let [baseInfo,created] = await orm.baseInfo.findOrCreate({
+                    where:{
+                        userApplicationId:post.userApplicationId,
+                        name:post.name,
+                        classPath:post.classPath,
+                        functionName:post.functionName
+                    },
+                    defaults:{
+                        userApplicationId:post.userApplicationId,
+                        name:post.name,
+                        classPath:post.classPath,
+                        functionName:post.functionName,
+                        keyPath:post.keyPath
+                    }
+                });
+                ctx.body = JSON.stringify({code:0,message:'成功'});
+            } else {
+                ctx.body = JSON.stringify({code:3,message:'参数错误'});
+            }
+        } else {
+            ctx.body = JSON.stringify({code:2,message:'token过期'});
+        }
+    }catch (e) {
+        ctx.body = JSON.stringify({code:1,error:e});
+    }
 };
 
 let update = async function (ctx) {
-
+    const header = {
+        token:ctx.request.headers.token
+    };
+    const post = {
+        userApplicationId:ctx.request.body.userApplicationId,
+        name:ctx.request.body.name,
+        classPath:ctx.request.body.classPath,
+        functionName:ctx.request.body.functionName,
+        keyPath:ctx.request.body.keyPath
+    };
+    try {
+        const payload = await jsonToken.verify(header.token);
+        if (jsonToken.isExpire(payload.iat)){
+            if (post.userApplicationId && post.name && post.classPath && post.functionName && post.keyPath) {
+                let baseInfo = await  orm.baseInfo.findOne({
+                    where:{
+                        userApplicationId:post.userApplicationId,
+                        name:post.name,
+                        classPath:post.classPath,
+                        functionName:post.functionName
+                    }
+                });
+                baseInfo.keyPath = post.keyPath;
+                baseInfo.save();
+                ctx.body = JSON.stringify({code:0,message:'成功'});
+            } else {
+                ctx.body = JSON.stringify({code:3,message:'参数错误'});
+            }
+        } else {
+            ctx.body = JSON.stringify({code:2,message:'token过期'});
+        }
+    }catch (e) {
+        ctx.body = JSON.stringify({code:1,error:e});
+    }
 };
 
 let list = async function (ctx) {
