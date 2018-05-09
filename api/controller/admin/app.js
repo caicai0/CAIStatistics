466e03db7588740526a9cf916e97bd7c5558a427
caicai <916903976@qq.com>
2018-05-09 10:26:30 +0800

    baseInfo 创建代码

--- api/controller/admin/app.js
+++ api/controller/admin/app.js
@@ -57,9 +57,67 @@ const createApp = async function (ctx) {
 };
 
 const updateApplication = async function (ctx) {
-    
+    const header = {
+        token:ctx.request.headers.token
+    };
+    const post = {
+        bundleIdentifier: ctx.request.body.bundleIdentifier,
+        platform: ctx.request.body.platform,
+        icon:ctx.request.body.icon
+    };
+    try {
+        const payload = await jsonToken.verify(header.token);
+        if (jsonToken.isExpire(payload.iat)){
+            const userId = payload.userId;
+            const userApplication = await orm.userApplication.findOne({where:{userId:userId,bundleIdentifier:post.bundleIdentifier,platform:post.platform}});
+            //这里可以修改信息的
+            if (userApplication) {
+                if (post.icon) {
+                    userApplication.icon = post.icon;
+                }
+                userApplication.save();
+                ctx.body = JSON.stringify({code: 0, message: '成功'});
+            }else {
+                ctx.body = JSON.stringify({code:3,message:'应用不存在'});
+            }
+        } else {
+            ctx.body = JSON.stringify({code:2,message:'token过期'});
+        }
+    }catch (e) {
+        ctx.body = JSON.stringify({code:1,error:e});
+    }
+};
+
+const deleteUserApplication = async function (ctx) {
+    //删除应用 和应用相关的内容 此处做真实删除  如果有必要后期添加历史数据库
+    const header = {
+        token:ctx.request.headers.token
+    };
+    const post = {
+        bundleIdentifier: ctx.request.body.bundleIdentifier,
+        platform: ctx.request.body.platform
+    };
+    try {
+        const payload = await jsonToken.verify(header.token);
+        if (jsonToken.isExpire(payload.iat)){
+            const userId = payload.userId;
+
+            let del = await orm.userApplication.delete({where:{userId:userId,bundleIdentifier:post.bundleIdentifier,platform:post.platform}});
+            if (del){
+                ctx.body = JSON.stringify({code: 0, message: '成功'});
+            } else {
+                ctx.body = JSON.stringify({code:3,message:'删除失败'});
+            }
+        } else {
+            ctx.body = JSON.stringify({code:2,message:'token过期'});
+        }
+    }catch (e) {
+        ctx.body = JSON.stringify({code:1,error:e});
+    }
 };
 
 module.exports = {
-    createApp
+    userApplicationList,
+    createApp,
+    updateApplication
 };
---
+++ api/controller/admin/baseInfo.js
@@ -0,0 +1,86 @@
+const orm = require('../../orm/models/index');
+const jsonToken = require('../../service/jsonToken');
+
+let create = async function (ctx) {
+    const header = {
+        token:ctx.request.headers.token
+    };
+    const post = {
+        userApplicationId:ctx.request.body.userApplicationId,
+        name:ctx.request.body.name,
+        classPath:ctx.request.body.classPath,
+        functionName:ctx.request.body.functionName,
+        keyPath:ctx.request.body.keyPath
+    };
+    try {
+        const payload = await jsonToken.verify(header.token);
+        if (jsonToken.isExpire(payload.iat)){
+            if (post.userApplicationId && post.name && post.classPath && post.functionName && post.keyPath) {
+                let [baseInfo,created] = await orm.baseInfo.findOrCreate({
+                    where:{
+                        userApplicationId:post.userApplicationId,
+                        name:post.name,
+                        classPath:post.classPath,
+                        functionName:post.functionName
+                    },
+                    defaults:{
+                        userApplicationId:post.userApplicationId,
+                        name:post.name,
+                        classPath:post.classPath,
+                        functionName:post.functionName,
+                        keyPath:post.keyPath
+                    }
+                });
+                ctx.body = JSON.stringify({code:0,message:'成功'});
+            } else {
+                ctx.body = JSON.stringify({code:3,message:'参数错误'});
+            }
+        } else {
+            ctx.body = JSON.stringify({code:2,message:'token过期'});
+        }
+    }catch (e) {
+        ctx.body = JSON.stringify({code:1,error:e});
+    }
+};
+
+let del = async function (ctx) {
+
+};
+
+let update = async function (ctx) {
+
+};
+
+let list = async function (ctx) {
+    const header = {
+        token:ctx.request.headers.token
+    };
+    const post = {
+        page:ctx.request.body.page,
+        size:ctx.request.body.size
+    };
+    try {
+        const payload = await jsonToken.verify(header.token);
+        if (jsonToken.isExpire(payload.iat)){
+            if (!post.page) {
+                post.page = 0;
+            }
+            if (!post.size) {
+                post.size = 20;
+            }
+            const list = await orm.userApplication.find({where:{userId:payload.userId},offset:post.page*post.size,limit:post.size});
+            ctx.body = JSON.stringify({code:0,data:list});
+        } else {
+            ctx.body = JSON.stringify({code:2,message:'token过期'});
+        }
+    }catch (e) {
+        ctx.body = JSON.stringify({code:1,error:e});
+    }
+};
+
+module.exports = {
+    create,
+    del,
+    update,
+    list
+};
